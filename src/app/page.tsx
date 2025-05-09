87ceb30e5c9245d3f74845ec8cdc8a529ddc50cd
Saltypeesh <tungthanh254@gmail.com>
2025-05-09 17:08:12 +0700

    Add Stream SDKs and update Convex API integration
    - Added @stream-io/node-sdk and @stream-io/video-react-sdk to package dependencies for enhanced video functionality.
    - Updated package.json and package-lock.json to reflect new dependencies.
    - Refactored Convex API to include http and users modules for improved API structure.
    - Replaced ClerkProvider with ConvexClerkProvider in layout for better integration with Convex.
    - Enhanced the home page to include user authentication buttons for signed-in and signed-out states.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,5 +3,14 @@ import { SignedIn, SignedOut, SignUpButton, UserButton } from "@clerk/nextjs";
 import { SignInButton } from "@clerk/nextjs";
 
 export default function Home() {
-  return <div className="m-10">Home</div>;
+  return (
+    <div className="m-10">
+      <SignedIn>
+        <UserButton />
+      </SignedIn>
+      <SignedOut>
+        <SignInButton />
+      </SignedOut>
+    </div>
+  );
 }
